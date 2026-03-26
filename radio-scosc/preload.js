169cfa81c2520bdd74f778cdf3de5130d58da0bf
tadashiusami <tadashiusami@oschub.asia>
2026-03-26 20:00:49 +0900

    Uodate renderer files

--- radio-scosc/preload.js
+++ radio-scosc/preload.js
@@ -3,5 +3,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
     onStatus: (cb) => ipcRenderer.on('status', (_, v) => cb(v)),
     onLog:    (cb) => ipcRenderer.on('log',    (_, v) => cb(v)),
-    joinRoom: (room, rate) => ipcRenderer.send('join-room', { room, rate }),
+    joinRoom: (hub, room, rate) => ipcRenderer.send('join-room', { hub, room, rate }),
 });
